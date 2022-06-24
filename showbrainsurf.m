9ca4e173faf5a0c6579ab3fa6e1de5c6e87b24c4
duddb3 <duddb3@p15-6294.chmccorp.cchmc.org>
2022-06-24 10:43:30 -0400

    Fixed path defintion for Gordon atlas

--- showbrainsurf.m
+++ showbrainsurf.m
@@ -177,7 +177,7 @@ function [LL,LM,RL,RM,S,I] = showbrainsurf(cdata,drange,surftype)
             tdir = fullfile(pth,'templates','templates_surfaces_32k');
             L = gifti(fullfile(tdir,['lh.' surfname]));
             R = gifti(fullfile(tdir,['rh.' surfname]));
-            gdir = 'C:\Users\duddb3\Documents\GordonAtlas\Parcels\';
+            gdir = fullfile(pth,'Gordon_atlas');
             GL = gifti(fullfile(gdir,'Parcels_L.func.gii'));
             GR = gifti(fullfile(gdir,'Parcels_R.func.gii'));
             if pickrois
